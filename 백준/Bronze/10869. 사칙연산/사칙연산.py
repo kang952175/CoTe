3755017d8e87067e707ff7a2648435cb27b17083
kang952175 <125178944+kang952175@users.noreply.github.com>
2023-12-07 20:08:06 +0900

    Update 사칙연산.py

--- 백준/Bronze/10869. 사칙연산/사칙연산.py
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -1,6 +1,17 @@
-a, b = map(int, input().split())
-print(a+b)
-print(a-b)
-print(a*b)
-print(a//b)
-print(a%b)
+def Arithmetic(a, b):
+    add = a + b
+    sub = a - b
+    mul = a * b
+    quotient = a // b
+    remainder = a % b
+    
+    return add, sub, mul, quotient, remainder
+
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    
+    answer = Arithmetic(a, b)
+    
+    for i in answer:
+        print(i)
+    
