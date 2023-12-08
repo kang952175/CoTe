0f94be8c85a53a226b109b884860405628bc5014
kang952175 <125178944+kang952175@users.noreply.github.com>
2023-12-08 17:32:45 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,17 @@
+def quadrant(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x <  0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+
+if __name__ == "__main__":
+    x = int(input())
+    y = int(input())
+    
+    answer = quadrant(x, y)
+    
+    print(answer)
