47cc0e54ce31fad1052ed6c237519fe309129334
kang952175 <125178944+kang952175@users.noreply.github.com>
2023-12-08 16:48:56 +0900

    [Bronze V] Title: 개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,9 @@
+def dog_emogi():
+    print("|\\_/|")
+    print("|q p|   /}")
+    print('( 0 )"""\\')
+    print('|"^"`    |')
+    print("||_/=\\\\__|")
+          
+if __name__ == "__main__":
+    dog_emogi()
