ff543590c23811e1861b44c48356a360604334b2
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-01-30 16:22:18 +0900

    Time: 1225 ms (27.27%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -8,4 +8,8 @@ FROM (
 ) AS event
 WHERE DATEDIFF(second_date, first_date) = 1;
 
-
+-- other solution 
+/* SELECT ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+IN (SELECT player_id, MIN(event_date) AS first_login FROM ACTIVITY GROUP BY player_id) */
