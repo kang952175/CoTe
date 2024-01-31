3d7bb2f091ae7b0d7d306bfb6997a9b5ab70c5bf
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-01-31 13:46:57 +0900

    Time: 543 ms (62.09%), Space: 0B (100%) - LeetHub

--- 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
+++ 0596-classes-more-than-5-students/0596-classes-more-than-5-students.sql
@@ -1,8 +1,4 @@
 select class
 from Courses
-where (student, class) in (
-    select student, class
-    from Courses
-    group by class
-    having count(student) >= 5 
-)
+group by class
+having count(student) >= 5 
