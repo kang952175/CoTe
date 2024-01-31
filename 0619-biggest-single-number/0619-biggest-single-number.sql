f0445fee1e331c992dbd1dba445c1590ca5df64e
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-01-31 18:31:59 +0900

    Time: 812 ms (40.98%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,6 +1,5 @@
 select max(num) as num
-from (
-    select num
-    from MyNumbers
-    group by num
-    having count(num) = 1) as single_number
+from MyNumbers
+where num in(select num from MyNumbers
+group by num
+having count(num)=1)
