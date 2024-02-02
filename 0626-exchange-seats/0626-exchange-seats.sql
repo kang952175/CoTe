0de2a6dfb3e95a356d0927414d7cc593ea3d00d5
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-02-02 16:13:20 +0900

    Time: 579 ms (84.05%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -9,3 +9,6 @@ FROM
 (SELECT *, COUNT(*) OVER() AS counts FROM Seat) AS Seat_alias
 ORDER BY id;
 
+-- SELECT ROW_NUMBER() OVER() id, student
+-- FROM seat
+-- ORDER BY IF(MOD(id, 2) = 0, id-1, id+1)
