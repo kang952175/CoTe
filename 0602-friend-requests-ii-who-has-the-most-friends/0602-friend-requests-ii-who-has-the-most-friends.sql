29f68ff8a712a12780d6991305091307eb608ec0
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-02-03 13:12:52 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,12 +1,13 @@
-SELECT id, SUM(count) AS num
-FROM
-((SELECT requester_id AS id, COUNT(requester_id) AS count
+with friends_count AS ()(SELECT requester_id AS id, COUNT(requester_id) AS count
     FROM RequestAccepted
     GROUP BY requester_id)
 UNION ALL
 (SELECT accepter_id AS id, COUNT(accepter_id) AS count
     FROM RequestAccepted
-    GROUP BY accepter_id)) friends_count
+    GROUP BY accepter_id))
+
+SELECT id, SUM(count) AS num
+FROM friends_count
 GROUP BY id
 ORDER BY num DESC
 LIMIT 1
