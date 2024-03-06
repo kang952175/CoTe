cc6f5004621bc2329bada937758f59f153e33ca9
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-03-06 19:36:34 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,7 +1,10 @@
-N = list(map(int, input().split()))
+# 체스 피스의 기대 개수
+expected = [1, 1, 2, 2, 2, 8]
 
-chess = [1, 1, 2, 2, 2, 8]
+# 사용자로부터 각 피스의 개수를 입력받음
+found = list(map(int, input().split()))
 
-missing_pieces = [str(chess[i] - N[i]) for i in range(len(chess))]
+# 각 피스의 기대 개수와 실제 개수의 차이를 계산하고 출력
+diff = [e - f for e, f in zip(expected, found)]
+print(*diff)
 
-print(" ".join(missing_pieces))
