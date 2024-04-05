0148b0473959bc6a42b7c603628b7f9ac2f5fceb
kang952175 <125178944+kang952175@users.noreply.github.com>
2024-04-05 15:24:57 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 21:35:55
+2024년 04월 05일 15:24:55
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,4 +1,4 @@
-import heapq
+import heapq as hq
 
 def solution(jobs):
     jobs.sort()
@@ -6,18 +6,18 @@ def solution(jobs):
     total_wait = 0
     current_time = 0
     wait_heap = []
-
+    
     while jobs or wait_heap:
         while jobs and jobs[0][0] <= current_time:
             start, duration = jobs.pop(0)
-            heapq.heappush(wait_heap, (duration, start))
+            hq.heappush(wait_heap, (duration, start))
         
         if wait_heap:
-            duration, start = heapq.heappop(wait_heap)
+            duration, start = hq.heappop(wait_heap)
             current_time += duration
             total_wait += current_time - start
         else:
             current_time = jobs[0][0]
-    
+        
     average_wait = total_wait // total_len  
     return average_wait
